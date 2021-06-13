5544f5165bb8989ee0b65f16ffd6bbff91721072
insanedude59 <80731924+insanedude59@users.noreply.github.com>
2021-06-13 19:28:10 -0400

    Update musclesimulator.lua

--- musclesimulator.lua
+++ musclesimulator.lua
@@ -2,22 +2,18 @@ local library = loadstring(game:HttpGet("https://remhub.net/upload/ui.lua"))() -
 local a = library:NewWindow("Shitty", " game")
 local section1 = a:NewSection("Autofarms")
 local section2 = a:NewSection("Player")
-section1:Toggle("Auto Lift", function(value)
+section1:Toggle("Autofarm", function(value)
     weightlift = value
-    while weightlift do wait()
-        game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Weight)
+    while weightlift do wait(0.3)
+ for i,v in pairs(game:GetDescendants()) do
+    if v.Name == "Pushups" and v.Parent ~= game.Players.LocalPlayer.Backpack then
     game:GetService("VirtualUser"):ClickButton1(Vector2.new())
+    elseif v.Parent == game.Players.LocalPlayer.Backpack and v.Name ~= "Combat" then
+    game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
+    end
+ end
  end
 end)
-section1:Toggle("Auto Pushup", function(value)
-    pushup = value
-    while pushup do wait()
-        game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Pushups)
-    game:GetService("VirtualUser"):ClickButton1(Vector2.new())
- end
-end)
-
-section1:Text("THE TOOLS NEED TO BE EQUIPPED")
 
 section2:Slider("WalkSpeed", 9,500,function(a)
     local player = game.Players.LocalPlayer
